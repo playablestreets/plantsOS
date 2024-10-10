ce5aaf478bf3bb41666b69866a1ce5578e79b65f
Bob Jarvis <bob@zeal.co>
2024-10-10 13:57:26 +1100

    reverting start change temporarily

--- scripts/start.sh
+++ scripts/start.sh
@@ -31,7 +31,8 @@
 sleep 5
 
 #Running Jack  --- ! changed -p (buffer) to 512, -n to 2, added -P for playback only
-jackd -P70 -p16 -t2000 -d alsa -dhw:Device -p 512 -n 2 -r 44100 -s -P &
+#jackd -P70 -p16 -t2000 -d alsa -dhw:Device -p 512 -n 2 -r 44100 -s -P &
+jackd -P70 -p16 -t2000 -d alsa -dhw:Device -p 128 -n 3 -r 44100 -s &
 
 # leave enough time for jack to start before launching PD
 sleep 10
