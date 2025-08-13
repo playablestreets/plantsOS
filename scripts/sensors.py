9330e5b84297a2124a088ba9f581723dd41c6a68
Bob Jarvis <bob@zeal.co>
2025-08-13 13:04:41 +1000

    faster sensor update

--- scripts/sensors.py
+++ scripts/sensors.py
@@ -1,8 +1,8 @@
 from time import sleep
 from pyOSC3 import OSCClient, OSCMessage
 from PiicoDev_LIS3DH import PiicoDev_LIS3DH
-from PiicoDev_CAP1203 import PiicoDev_CAP1203 # Import the CAP1203 library
-from PiicoDev_Unified import sleep_ms # cross-platform compatible sleep function
+from PiicoDev_CAP1203 import PiicoDev_CAP1203
+from PiicoDev_Unified import sleep_ms
 
 # Initialize the two PiicoDev boards
 motion = PiicoDev_LIS3DH()
@@ -10,34 +10,41 @@ touch_sensor = PiicoDev_CAP1203(touchmode='single', sensitivity=6)
 
 # Set up the OSC client
 client = OSCClient()
-client.connect( ('127.0.0.1', 8880) ) # Connect to the OSC receiver
+client.connect( ('127.0.0.1', 8880) )
 
 print("Sending LIS3DH and CAP1203 data to localhost:8880. Press Ctrl+C to stop.")
 
+# Initialize previous values to None
+prev_x, prev_y, prev_z = None, None, None
+prev_status = [None, None, None]
+
 while True:
-	# Get data from the LIS3DH Gyro
-	x, y, z = motion.angle # Tilt could be measured with respect to three different axes
-	
-	msg_tilt = OSCMessage("/tilt");
-	msg_tilt.append(x,'f')
-	msg_tilt.append(y,'f')
-	msg_tilt.append(z,'f')
+    # Get data from the LIS3DH Gyro
+    x, y, z = motion.angle
+    
+    # Check if any tilt value has changed
+    if x != prev_x or y != prev_y or z != prev_z:
+        msg_tilt = OSCMessage("/tilt")
+        msg_tilt.append(x, 'f')
+        msg_tilt.append(y, 'f')
+        msg_tilt.append(z, 'f')
+        client.send(msg_tilt)
+        
+        # Update previous values
+        prev_x, prev_y, prev_z = x, y, z
 
-	client.send(msg_tilt)
+    # Get data from the CAP1203 Touch Sensor
+    status = touch_sensor.read()
+    
+    # Check if any touch status has changed
+    if status[1] != prev_status[0] or status[2] != prev_status[1] or status[3] != prev_status[2]:
+        msg_touch = OSCMessage("/touch")
+        msg_touch.append(float(status[1]), 'f')
+        msg_touch.append(float(status[2]), 'f')
+        msg_touch.append(float(status[3]), 'f')
+        client.send(msg_touch)
 
-	# Get data from the CAP1203 Touch Sensor
-	status = touch_sensor.read() # Read the touch status of the three pads (returns a list of booleans)
-
-	# print(status)
-
-	msg_touch = OSCMessage("/touch");
-	msg_touch.append(float(status[1]), 'f') # Pad 1
-	msg_touch.append(float(status[2]), 'f') # Pad 2
-	msg_touch.append(float(status[3]), 'f') # Pad 3
-
-	client.send(msg_touch)
-
-	# Print data
-	# print(f"X: {x: .2f}, Y: {y: .2f}, Z: {z: .2f} | Touch: {status}")
-	
-	sleep(0.1) # Wait 100 milliseconds
+        # Update previous values
+        prev_status = [status[1], status[2], status[3]]
+    
+    sleep(0.02)
