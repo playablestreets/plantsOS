3d5aad54b653b4a365f46cfd6ba3c81755a7bc74
Bob Jarvis <bob@zeal.co>
2025-08-14 09:49:30 +1000

    sample updating script

--- scripts/updatesamples.sh
+++ scripts/updatesamples.sh
@@ -1,23 +1,56 @@
 #!/bin/bash
 
-# todo REQUIRES UPDATING TO CONFORM TO BOP FOLDER STRUCTURE
+# Source the configuration file to access variables
+source "$(dirname "$0")/bop.config"
 
-# cd /home/pi
-# # todo get samples.zip from somewhere - ftp? scp? curl? wget?
+# Get the absolute path to the git repository root
+GITREPO_ROOT=$(git -C "$(dirname "$0")" rev-parse --show-toplevel)
 
-# mkdir /home/pi/efm-temp
-# cd /home/pi/efm-temp
-# cp /home/pi/enormousFM/samples/README.md ./
-# cp /home/pi/enormousFM/samples/.gitignore ./
-# rm -rf /home/pi/enormousFM/samples
-# mkdir /home/pi/enormousFM/samples
-# unzip /home/pi/samples.zip -d /home/pi/enormousFM/samples
-# rm -r /home/pi/enormousFM/samples/__MACOSX
-# mv /home/pi/efm-temp/README.md /home/pi/enormousFM/samples/
-# mv /home/pi/efm-temp/.gitignore /home/pi/enormousFM/samples/
-# rm -r /home/pi/efm-temp
-# #set permissions
-# cd /home/pi/enormousFM/samples
-# chown -R pi ./
+# Define the full path for the sample packs directory
+SAMPLES_DIR_PATH="$GITREPO_ROOT/$SAMPLEPACKSDIR"
 
-exit
+# Define a temporary file for the downloaded zip
+TEMP_ZIP_FILE=$(mktemp)
+
+# Get the current user
+CURRENT_USER=$(whoami)
+
+echo "Downloading sample packs from $SAMPLESPACKSURL..."
+# Download the zip file
+curl -L -o "$TEMP_ZIP_FILE" "$SAMPLESPACKSURL"
+
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download sample packs."
+    rm "$TEMP_ZIP_FILE"
+    exit 1
+fi
+
+echo "Cleaning up existing sample packs directory: $SAMPLES_DIR_PATH"
+# Remove the existing directory and its contents
+rm -rf "$SAMPLES_DIR_PATH"
+
+echo "Creating new directory: $SAMPLES_DIR_PATH"
+# Create the new, empty directory
+mkdir -p "$SAMPLES_DIR_PATH"
+
+echo "Extracting sample packs to $SAMPLES_DIR_PATH..."
+# Unzip the downloaded file directly into the new directory
+unzip -o "$TEMP_ZIP_FILE" -d "$SAMPLES_DIR_PATH"
+
+# Check if the unzip was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to extract sample packs."
+    rm "$TEMP_ZIP_FILE"
+    exit 1
+fi
+
+echo "Setting ownership of $SAMPLES_DIR_PATH to $CURRENT_USER..."
+# Change the owner of the directory and its contents recursively
+chown -R "$CURRENT_USER":"$CURRENT_USER" "$SAMPLES_DIR_PATH"
+
+# Clean up the temporary zip file
+rm "$TEMP_ZIP_FILE"
+
+echo "Sample packs updated and ownership set successfully!"
+exit 0
